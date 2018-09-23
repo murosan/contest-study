2e48d0878209da0597c9ebbff059ca997d8f5ebd
murosan <m.murota.dev@gmail.com>
2018-09-23 17:53:16 +0900

    login.js

--- tool/puppeteer/login.js
+++ tool/puppeteer/login.js
@@ -1,12 +1,32 @@
 "use strict";
 
+const path = require("path");
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
+const config = require("../config.json");
+const loginURL = "https://practice.contest.atcoder.jp/login";
+const settings = { headless: false, slowMo: 20 };
+const cookieFile = path.resolve(__dirname, "../cookies/atcoder.json");
 
+(async () => {
+  const browser = await puppeteer.launch(settings);
+  const page = await browser.newPage();
+  await page.goto(loginURL);
+
+  await page.focus('input[name="name"]');
+  await page.type('input[name="name"]', config.username);
+  await page.focus('input[name="password"]');
+  await page.type('input[name="password"]', config.password);
+  const btn = await page.$("button[type=submit]");
+  await btn.click();
+
+  await page.waitForNavigation({
+    timeout: 60000,
+    waitUntil: "domcontentloaded"
+  });
+
+  const cookie = await page.cookies();
+  fs.writeFileSync(cookieFile, JSON.stringify(cookie));
   await browser.close();
 })();
